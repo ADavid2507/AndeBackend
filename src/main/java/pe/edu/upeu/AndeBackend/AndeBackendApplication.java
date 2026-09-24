7415932eb1f72bd65dc2a41671701b99b86686c7
ADavid2507 <calebasmat2018@gmail.com>
2026-09-24 09:00:59 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upeu/AndeBackend/AndeBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.AndeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndeBackendApplication.class, args);
+	}
+
+}
