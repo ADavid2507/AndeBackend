f990d8e0e10cee2a0079551bdc3ef13bbc2a4108
ADavid2507 <calebasmat2018@gmail.com>
2026-09-24 09:34:01 -0500

    carrera y curso repositoy y service creados

--- src/main/java/pe/edu/upeu/AndeBackend/service/service/CarreraService.java
+++ src/main/java/pe/edu/upeu/AndeBackend/service/service/CarreraService.java
@@ -1,4 +1,8 @@
 package pe.edu.upeu.AndeBackend.service.service;
 
-public interface CarreraService {
+import pe.edu.upeu.AndeBackend.dto.CarreraRequestDTO;
+import pe.edu.upeu.AndeBackend.dto.CarreraResponseDTO;
+import pe.edu.upeu.AndeBackend.service.generic.CrudService;
+
+public interface CarreraService extends CrudService<CarreraRequestDTO, CarreraResponseDTO, Long> {
 }
--- src/main/java/pe/edu/upeu/AndeBackend/service/service/CursoService.java
+++ src/main/java/pe/edu/upeu/AndeBackend/service/service/CursoService.java
@@ -1,4 +1,8 @@
 package pe.edu.upeu.AndeBackend.service.service;
 
-public interface CursoService {
+import pe.edu.upeu.AndeBackend.dto.CursoRequestDTO;
+import pe.edu.upeu.AndeBackend.dto.CursoResponseDTO;
+import pe.edu.upeu.AndeBackend.service.generic.CrudService;
+
+public interface CursoService extends CrudService<CursoRequestDTO, CursoResponseDTO, Long> {
 }
